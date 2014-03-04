7bf64470bbc87f86ed7d67c61e0984154e4a37a0
Samuel McTaggart <sam.mctaggart@gmail.com>
2014-03-04 10:08:54 -0500

    reset parameters

--- app/controllers/games/challenge.js
+++ app/controllers/games/challenge.js
@@ -8,31 +8,39 @@ App.GamesChallengeController = Ember.Controller.extend({
   score: 0,
   lastRaise: 0,
   streak: 0,
+  wrongStreak: 0,
   pointValue: 1,
   correct: false,
-  originalParameters: {operators: ['+'], min: 1, max: 10, combinations: 1},
   parameters: {operators: ['+'], min: 1, max: 10, combinations: 1},
   playing: false,
-  difficultyGap: 1,
+  difficultyGap: 3,
 
   question: function(){
     var parameters = this.get('parameters');
-    var modifier = this.get('difficultyGap') * 10;
+    var modifier = this.get('gameDifficulty') * 10;
     this.send('makeQuestion', modifier, parameters, 'gamesChallenge');
     return this.get('currentQuestion');
   }.property('playing', 'currentQuestion'),
 
   difficulty: function(){
     if (this.get('streak') - (this.get('lastRaise')) >= this.get('difficultyGap')){
-      var parameters = this.get('parameters');
-      this.send('raiseDifficulty');
       this.incrementProperty('gameDifficulty');
+      this.send('raiseDifficulty');
       this.set('lastRaise', this.get('streak'));
+      return this.get('gameDifficulty');
+    } else if (this.get('streak') > 0){
+      this.send('raiseDifficulty');
+      return this.get('gameDifficulty');
+    } else if (this.get('wrongStreak') >= 2){
+      this.send('lowerDifficulty');
+      this.set('wrongStreak', 0);
+      this.set('lastRaise', 0);
+      this.set('currentQuestion', null);
       return this.get('gameDifficulty');
     } else {
       return this.get('gameDifficulty');
     }
-  }.property('streak', 'score'),
+  }.property('wrongStreak', 'streak', 'score'),
 
   gameOver: function(){
     if (this.get('timeLeft') <= 0){
@@ -49,10 +57,12 @@ App.GamesChallengeController = Ember.Controller.extend({
       this.set('streak', 0);
       this.set('pointValue', 1);
       this.set('lastRaise', 0);
+      this.set('wrongStreak', 0);
+      this.set('difficultyGap', 3);
       this.set('gameDifficulty', 1);
       this.set('timePerQuestion', 10);
-      this.set('parameters', this.get('originalParameters'));
       this.toggleProperty('playing');
+      this.send('resetParameters');
       this.send('countDown');
     },
     countDown: function(){
@@ -61,6 +71,11 @@ App.GamesChallengeController = Ember.Controller.extend({
       _this.decrementProperty('timeLeft')
       }, 1000);
     },
+
+    resetParameters: function(){
+      this.set('parameters', {operators: ['+'], min: 1, max: 10, combinations: 1});
+    },
+    
     restart: function(){
       this.toggleProperty('playing');
       clearInterval(this.timer);
@@ -73,9 +88,11 @@ App.GamesChallengeController = Ember.Controller.extend({
         this.incrementProperty('streak');
         this.set('timeLeft', this.get('timePerQuestion'));
         this.set('score', this.get('score') + this.get('pointValue'));
+        this.set('wrongStreak', 0);
         this.toggleProperty('correct');
       } else {
         this.set('streak', 0);
+        this.incrementProperty('wrongStreak');
       }
     }
   }
--- app/routes/games/challenge.js
+++ app/routes/games/challenge.js
@@ -10,16 +10,16 @@ App.GamesChallengeRoute = Ember.Route.extend({
         parameters['max'] += 2;
       };
       if (streak % 5 === 0){
-        this.send('lowerTime');
+        this.send('changeTime', 0.92);
         this.send('raiseValue');
       };
       switch(true){
-        case (score > 25):
+        case (score > 25 && streak > 2):
           if (parameters['combinations'] < 3){
             this.send('addCombination');
           }
           break;
-        case (score > 15):
+        case (score > 15 && streak > 2):
           if (parameters['combinations'] < 2){
             this.send('addCombination');
           }
@@ -39,12 +39,26 @@ App.GamesChallengeRoute = Ember.Route.extend({
         break;
       }
     },
+    lowerDifficulty: function(){
+      var difficulty = this.controller.get('gameDifficulty');
+      if (difficulty > 1){
+        this.controller.decrementProperty('gameDifficulty');
+      }
+      var pointValue = this.controller.get('pointValue');
+      if (pointValue > 1){
+        this.controller.decrementProperty('pointValue');
+      }
+      var parameters = this.controller.get('parameters');
+      if (parameters['max'] >= 8){
+        parameters['max'] -= 2;
+      }
+    },
     raiseValue: function(){
       this.controller.incrementProperty('pointValue');
     },
-    lowerTime: function(){
+    changeTime: function(modifier){
       var timePerQuestion = this.controller.get('timePerQuestion');
-      this.controller.set('timePerQuestion', Math.round(timePerQuestion * 0.92));
+      this.controller.set('timePerQuestion', Math.round(timePerQuestion * modifier));
     },
     addCombination: function(){
       var parameters = this.controller.get('parameters');
